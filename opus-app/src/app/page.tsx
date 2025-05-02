7631346bbafbf456a77174dc0afbc4cef071a32f
LoserAim <drew.doser@gmail.com>
2025-05-02 13:17:56 -0700

    adding user models for testing, useAxios hook for retrieval

--- opus-app/src/app/page.tsx
+++ opus-app/src/app/page.tsx
@@ -1,6 +1,15 @@
+"use client"
+
+import UseAxios from "../_hooks/UseAxios";
+import {UserModel} from "@/_models/user-model";
 
 export default function Home() {
+  const {data, loading, error} = UseAxios<UserModel[]>("/users","get")
+
   return (
-   <h1>hello world</h1>
+      <>
+        <h1>testing{!loading && JSON.stringify(data)}</h1>
+        <h2>testing{!loading && JSON.stringify(error)}</h2>
+      </>
   );
 }
